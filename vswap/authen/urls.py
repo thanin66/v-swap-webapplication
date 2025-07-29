ead82dc862b80f1578a47c4f9e53b58a8eb356f3
thanin66 <thanin.ra.66@ubu.ac.th>
2025-07-29 20:22:40 +0700

    first commit

---
+++ vswap/authen/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("register/", views.register_view, name="register"),
+    path("", views.login_view, name="login"),
+    path("logout/", views.logout_view, name="logout"),
+    path("home/", views.home_view, name="home"),
+]
---
+++ vswap/authen/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate, logout
+from .forms import CustomUserCreationForm
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.decorators import login_required
+
+def register_view(request):
+    if request.method == "POST":
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("home")
+    else:
+        form = CustomUserCreationForm()
+    return render(request, "authen/register.html", {"form": form})
+
+def login_view(request):
+    if request.method == "POST":
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect("home")
+    else:
+        form = AuthenticationForm()
+    return render(request, "authen/login.html", {"form": form})
+
+def logout_view(request):
+    logout(request)
+    return redirect("login")
+
+
+@login_required
+def home_view(request):
+    return render(request, "authen/home.html")
