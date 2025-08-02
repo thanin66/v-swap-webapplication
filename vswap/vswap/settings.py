2a7740ca2c4e8840a20bd7e2b41376da7a6787f7
thanin66 <thanin.ra.66@ubu.ac.th>
2025-08-02 12:00:26 +0700

    connect DB

--- vswap/vswap/settings.py
+++ vswap/vswap/settings.py
@@ -77,8 +77,12 @@ LOGOUT_REDIRECT_URL = '/login/'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'vswapDB',
+        'USER': 'postgres',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
